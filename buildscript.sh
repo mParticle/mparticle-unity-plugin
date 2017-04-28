ea809324a21ce1ec19239c677b936b383068432d
Sam Dozor <sdozor@mparticle.com>
2017-04-28 11:40:21 -0400

    Rename Android artifact

--- buildscript.sh
+++ buildscript.sh
@@ -7,7 +7,7 @@ project_dir=${PWD}
     -exportPackage Assets/mParticle/MParticle.cs \
                    Assets/mParticle/MParticleAndroid.cs \
                    Assets/mParticle/MParticleiOS.cs \
-                   Assets/Plugins/Android/android-core-4.12.0.aar \
+                   Assets/Plugins/Android/mParticle-core-4.12.0.aar \
                    Assets/Plugins/iOS/MParticleUnity.h \
                    Assets/Plugins/iOS/MParticleUnity.m \
                    Assets/Plugins/iOS/libmParticle-Apple-SDK.a \
